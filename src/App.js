fb3d9e337a1ac9cab1a7785e8d66c1f9d332b773
Emmanuel Onah <emmajerry2016@gmail.com>
2020-10-18 20:37:21 +0300

    splitted routing code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import LandingPage from "./pages/landing/LandingPage.component";
-import Shop from './pages/shop/Shop.component'
-import Product from './pages/product/Product.component'
-import NotFound from "./pages/notfound/NotFound.component";
 import "./app.style.scss";
+
+const LandingPage = lazy(() => import("./pages/landing/LandingPage.component"));
+const Shop = lazy(() => import("./pages/shop/Shop.component"));
+const Product = lazy(() => import("./pages/product/Product.component"));
+const NotFound = lazy(() => import("./pages/notfound/NotFound.component"));
 
 const App = () => {
     return (
         <Router>
-            <Switch>
-                <Route exact path="/" component={LandingPage} />
-                <Route exact path="/shop" component={Shop} />
-                <Route exact path="/product/:productId" component={Product} />
-                <Route component={NotFound} />
-            </Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route exact path="/" component={LandingPage} />
+                    <Route exact path="/shop" component={Shop} />
+                    <Route exact path="/product/:productId" component={Product} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Suspense>
         </Router>
     );
 };
